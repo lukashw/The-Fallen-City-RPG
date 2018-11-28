fe50789dc45794b4211c8b331c7f682e38d19456
Lukas Williams <lukashensel-williams@Lukass-MacBook-Pro.local>
2018-11-28 16:34:09 -0500

    Added main game screen and player
    added main game screen and player and player movement

---
+++ rpgGame/theFallenCity/theFallenCity/Load/MainGame/GamePlay.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using theFallenCity.MainGame;
+
+namespace theFallenCity
+{
+
+    public class GamePlay: gameScreen
+    {
+        Player player;
+
+        public GamePlay()
+        {
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            xmlManger<Player> playerLoader = new xmlManger<Player>();
+            player = playerLoader.Load("Load/MainGame/Player.xml");
+            player.LoadContent();
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+            player.UnloadContent();
+        }
+
+        public override void Update(GameTime gametime)
+        {
+            base.Update(gametime);
+            player.Update(gametime);
+        }
+
+        public override void Draw(SpriteBatch spritebatch)
+        {
+            base.Draw(spritebatch);
+            player.Draw(spritebatch);
+        }
+    }
+}
---
+++ rpgGame/theFallenCity/theFallenCity/Load/MainGame/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace theFallenCity.MainGame
+{
+    public class Player
+    {
+        public Vector2 Velocity;
+        public Image Image;
+        public float MoveSpeed;
+
+        public Player()
+        {
+            Velocity = Vector2.Zero;
+        }
+
+        public void LoadContent()
+        {
+            Image.LoadContent();
+        }
+
+        public  void UnloadContent()
+        {
+            Image.UnloadContent();
+        }
+
+        public  void Update(GameTime gametime)
+        {
+            //(player movement) if diagonal momvent is wanted then remove the if statements
+            if (Velocity.X == 0)
+            {
+                if (InputManager.Instance.KeyDown(Keys.S))
+                    //down
+                    Velocity.Y = MoveSpeed * (float)gametime.ElapsedGameTime.TotalSeconds;
+                else if (InputManager.Instance.KeyDown(Keys.W))
+                    //up
+                    Velocity.Y = -MoveSpeed * (float)gametime.ElapsedGameTime.TotalSeconds;
+                else
+                    Velocity.Y = 0;
+            }
+
+            if(Velocity.Y ==0)
+            {
+            if (InputManager.Instance.KeyDown(Keys.D))
+                //left
+                Velocity.X = MoveSpeed * (float)gametime.ElapsedGameTime.TotalSeconds;
+            else if (InputManager.Instance.KeyDown(Keys.A))
+                //right
+                Velocity.X = -MoveSpeed * (float)gametime.ElapsedGameTime.TotalSeconds;
+            else
+                Velocity.X = 0;
+            }
+            Image.Position += Velocity;
+
+        }
+
+        public  void Draw(SpriteBatch spritebatch)
+        {
+            Image.Draw(spritebatch);
+        }
+    }
+}
